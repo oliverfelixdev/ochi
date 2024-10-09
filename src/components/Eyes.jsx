e7b20e186f1bf849bce397225cf654b3c035e5af
oliverfelixdev <oliverfelix.dev@gmail.com>
2024-10-09 16:16:04 +0530

    Eyes Interection

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 function Eyes() {
+  const [rotate, setRotate] = useState(0);
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - window.innerWidth/2
+      let deltaY = mouseY - window.innerHeight/2
+
+      let angle = Math.atan2(deltaY, deltaX) * (180/Math.PI)
+      setRotate(angle-180)
+    })
+  })
+
   return (
     <div className='eyes w-full h-screen overflow-hidden'>
-        <div className="eyes-cocuk"></div>
+      <div className="main-wrap">
+        <div className="eyes-wrap">
+
+          <div className="eye-main">
+            <div className="w-2/3 h-2/3 rounded-full bg-[#212121] relative overflow-hidden">
+              <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line w-[96%] h-5 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+                <div className="h-5 w-5 rounded-full bg-[#f1f1f1] flex items-center justify-center"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="eye-main">
+            <div className="w-2/3 h-2/3 rounded-full bg-[#212121] relative overflow-hidden">
+              <div style={{transform: `translate(-50%, -50%) rotate(${rotate}deg)`}} className="line w-[96%] h-5 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+                <div className="h-5 w-5 rounded-full bg-[#f1f1f1] flex items-center justify-center"></div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
