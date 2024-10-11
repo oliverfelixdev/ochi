197678106ee799b588799fcbdc4b756f0f2d60c5
oliverfelixdev <oliverfelix.dev@gmail.com>
2024-10-11 17:22:40 +0530

    previous changes updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Eyes from './components/Eyes'
+import Featured from './components/Featured'
 function App() {
   return (
     <div className='web-screen'>
@@ -13,6 +14,7 @@ function App() {
       <Marquee />
       <About />
       <Eyes />
+      <Featured />
     </div>
   )
 }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className='w-full h-screen'>
+        <div className="wrap px-12">
+            <h1 className="title-featured text-5xl pt-20">Featured projects</h1>
+        </div>
+        <hr className='h-[2px] w-full bg-[#D2D2D2] mt-10'/>
+    </div>
+  )
+}
+
+export default Featured
